359477102f5ada0a2765f218c49dac3a829a0cac
Aaron Rumery <arumery@Aarons-MacBook-Pro.local>
2024-11-21 18:02:00 -0500

    feat: static deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: cloudflare(),
+  output: 'static',
   site: 'https://riverviewsubdivision.com',
   image: {
     service: {
